f15e7c7803f1177b3ca60bc76a8444086790c38b
Daniel.Brodsky <brod.daniel@gmail.com>
2021-04-08 15:57:02 +0300

    new task

--- source_files/Pqueue.c
+++
@@ -1,8 +0,0 @@
-/**********************************FILE-HEADER*********************************\
-* File: Pqueue.c						 		  								
-* Author: Daniel Brodsky					  								
-* Date: 29/03/2021							   								
-* Version: 1.0 (After Review)					   								
-* Reviewer: Ariel							   								
-* Description: Priority Queue implementation wrapped by Sorted List's API.			 
-\******************************************************************************/
