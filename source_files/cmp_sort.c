15df8f2a5a8cb94264729a4e5503ab8f8e5d30e2
Daniel.Brodsky <brod.daniel@gmail.com>
2021-05-12 12:09:51 +0300

    added SelectionSort

--- source_files/cmp_sort.c
+++ source_files/cmp_sort.c
@@ -49,7 +49,34 @@ void BubbleSort(int *arr, size_t size)
 
 void SelectionSort(int *arr, size_t size)
 {
+		/*
+	
+	assert and verify that the given array paramater is valid and not null.
 
+	check if the size of the array is bigger than one. if it's not, do nothing.
+	
+	run through the array and divide it to "sorted" part and "unsorted" part.
+	The "sorted" part starts at index 0, promote this index by one.
+	Each iteration will swap the smallest item in the "unsorted" part with
+	the item that currently located at the "sorted" index.
+
+	size_t sorted_item = 0;    
+	size_t unsorted_item = 0;
+	size_t smallest_value = arr[sorted_item]; # stores index of smallest item
+	
+	while (sorted_item <= size - 1):
+		unsorted_item = sorted_item + 1;
+		
+		while (unsorted_item < size):
+				if (arr[unsorted_item] < arr[smallest_value]):
+					arr[smallest_value] = arr[unsorted_item];
+				++unsorted_item;
+				
+		arr[sorted_item] = arr[smallest_value];
+		++sorted_item;
+		
+			
+	*/   
 }
 
 /******************************************************************************/
