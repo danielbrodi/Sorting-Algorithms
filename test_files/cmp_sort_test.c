93fef52a0eb2702556be9084478452c73f155022
danielbrodi <broddanik@gmail.com>
2021-05-13 10:38:17 +0300

    changed test functions to static. added fillup and copy funcs. fill up random numbers

--- test_files/cmp_sort_test.c
+++ test_files/cmp_sort_test.c
@@ -6,44 +6,96 @@
 * Description:		Sorting Algorithms implementations testing file.		 
 \******************************************************************************/
 
-#include <stddef.h>				/*	size_t, NULL	*/
-#include <stdio.h>
+/********************************* Inclusions *********************************/
+#include <stddef.h>	/*	size_t			*/
+#include <stdio.h>	/*	printf			*/
+#include <stdlib.h>	/*	rand, srand		*/
+#include <time.h>	/*	time			*/
 
+#include "utils.h"	/*	ANSI_COLOR_		*/
 #include "cmp_sort.h"
 
-/* Function to print an array */
-void printArray(int arr[], size_t size)
-{
-	int i;
-	for (i=0; i < (int)size; i++)
-	printf("%d ", arr[i]);
-	printf("\n");
-}
+/***************************** Macros Definitions *****************************/
+/* print colored output */
+#define PRINT_SUCCESS printf (ANSI_COLOR_GREEN "SUCCESS\n" ANSI_COLOR_RESET)
+#define PRINT_FAILURE printf (ANSI_COLOR_RED "FAILURE\n" ANSI_COLOR_RESET)
+
+/* generates random number from 1 to 100 */
+#define RANDOM_NUM ((rand() % 200 - 100))	
+
+/**************************** Forward Declarations ****************************/
+static void PrintArray(int arr[], size_t size);
+static void FillUpArray(int *arr, size_t size);
+static void CopyArray(int *arr1, int *arr2, size_t size);
+
+/******************************************************************************/
+/******************************* Main__Function *******************************/
 
 int main()
-{
-	int arr1[] = {64, 34, 25, 12, 22, 11, 25, 90, 0, -2};
-	int arr2[] = {64, 34, 25, 12, 22, 11, 25, 90, 0, -2};
-	int arr3[] = {64, 34, 25, 12, 22, 11, 25, 90, 0, -2};
-
-	size_t n = sizeof(arr1)/sizeof(arr1[0]);
-
+{	
+	int arr1[10] = {0};
+	int arr2[10] = {0};
+	int arr3[10] = {0};
+	
+	size_t size = sizeof(arr1)/sizeof(arr1[0]);
+	
+	/*	Intializes a random number generator	*/
+	srand(time(0));
+	
+	FillUpArray(arr1, size);
+	
+	CopyArray(arr1, arr2, size);
+	CopyArray(arr1, arr3, size);
+	
 	printf("Original array: ");
-	printArray(arr1, n);
+	PrintArray(arr1, size);
 
-	BubbleSort(arr1, n);
-	SelectionSort(arr2,n);
-	InsertionSort(arr3,n);
+	BubbleSort(arr1, size);
+	SelectionSort(arr2,size);
+	InsertionSort(arr3,size);
 
 
 	printf("Bubble Sort:    ");
-	printArray(arr1, n);
+	PrintArray(arr1, size);
 
 	printf("Selection Sort: ");
-	printArray(arr2, n);
+	PrintArray(arr2, size);
 
 	printf("Insertion Sort: ");
-	printArray(arr3, n);
+	PrintArray(arr3, size);
 
 	return 0;
 }
+/******************************************************************************/
+static void PrintArray(int arr[], size_t size)
+{
+	size_t i = 0;
+	
+	for (i = 0; i < size; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	
+	printf("\n");
+}
+/******************************************************************************/
+static void FillUpArray(int *arr, size_t size) 
+{
+	size_t i = 0;
+	
+	for(i = 0; i < size; ++i)
+	{
+		arr[i] = RANDOM_NUM;
+	}
+}
+/******************************************************************************/
+static void CopyArray(int *arr1, int *arr2, size_t size)
+{
+	size_t i = 0;
+	
+	for(i = 0; i < size; ++i)
+	{
+		arr2[i] = arr1[i];
+	}
+}
+/******************************************************************************/
