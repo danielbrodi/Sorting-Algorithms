f5aa7f1e71978965c8298b752ab469d659c9a1a6
Daniel.Brodsky <brod.daniel@gmail.com>
2021-05-24 01:07:20 +0300

    ready radix pseudo

--- source_files/radix.c
+++ source_files/radix.c
@@ -14,21 +14,7 @@
 void CountingSortIMP(pair_ty *dest, pair_ty *src, size_t num_of_pairs,
 							size_t *histogram, size_t from_bit, size_t to_bit)
 {
-	/*
-		Iterate the src array and find the maximum value present in it.
-		Declare a new counting array of size max+1 with value 0.
-		Count each and every element in the array and increment its value 
-		at the right index in the counting array.
-		Find cumulative sum is the counting array we adding curr and 
-		prev frequency.
-		Now the cumulative value actually signifies the actual location
-		of the element in the sorted dest array.
-		Start iterating counting array from 0 to max:
-			Put 0 at the right index and reduce the count by 1, 
-			which will signify the second position of the element if it
-			exists in the input array.
-		Now transfer array received in the above step in the dest input array.
-	*/
+	/* */
 }
 
 /* returns non-zero if fail to allocate memory 					*/
@@ -36,11 +22,20 @@ void CountingSortIMP(pair_ty *dest, pair_ty *src, size_t num_of_pairs,
 int RadixSort(void *dest, void *src, size_t num_of_elements, size_t element_size,
 					ConvertFunc DataToKey, size_t msb, size_t num_of_digits)
 {
-	/*
-		init src and dest arrays, lets call them input[] & output[]:
-			create pair_ty arr of size of num_of_elements * element_size;
-		
-		
-		
-	*/
+	/*	assert*/
+
+	/*	create histogram array of size of (msb + 1 / num_of_digits)			*/
+	/*	handle errors if any												*/
+
+	/*	create 2 pair_ty arrays, handle errors if any						*/
+
+	/*	for each element in src arr: 										*/
+	/*		copy ptr and extracted key to src_pair array					*/
+	
+	/*	for num_of_digits: 													*/
+	/*		nullify histogram												*/
+	/*		call CountingSortIMP with current subset of bits				*/
+	/*		switch src_pair and dest_pair arrays							*/
+	/*		promote bit indexes to next subset								*/
+	
 }
